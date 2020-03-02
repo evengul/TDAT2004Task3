044629aa131710090e111d0845b536ab637fb7e7
LAPTOP-6SJOR7OT\Even <evengu@stud.ntnu.no>
2020-03-02 12:46:22 +0100

    Switched to ShellJS

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ shell.exec('docker build -t my-gcc-app .');
 app.post("/compilecpp", (request, response) => {
 
     console.log("Request to compile C++");
-    let output = shell.exec("docker run --rm --name=compiler -a STDOUT -a STDERR my-gcc-app -e \"CONTENT=\"" + request.body.toCompile + "\"\"");
+    let output = shell.exec("docker run --rm --name=compiler -a STDOUT -a STDERR my-gcc-app ");
     if (output.stderr === '' && output.code === 0){
         response.status(200);
         response.json({result: output});
